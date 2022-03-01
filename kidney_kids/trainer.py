9991804cde83ce9e6e6a408f296140227bd6f24c
Viktoria von Laer <viktoria.von.laer@hotmail.de>
2022-03-01 15:45:27 +0100

    added class files for models, add model notebook

---
+++ kidney_kids/trainer.py
@@ -0,0 +1,21 @@
+class Trainer():
+    '''logreg:
+    ['age'] -> standard
+    ['bp'] -> minmax
+    ['bgr'] -> minmax
+    ['bu'] -> minmax
+    ...
+    ['hemo'] -> standard
+
+    '''
+    '''knn:
+    all -> minmax
+    '''
+    '''tree:
+    ['age'] -> standard
+    ['bp'] -> minmax
+    ['bgr'] -> minmax
+    ['bu'] -> minmax
+    ...
+    ['hemo'] -> standard
+    '''
