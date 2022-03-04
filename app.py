235859cad02c655e3745464460a8064555fe4b26
cristian jeraldo <cristian.jeraldo.uk@hotmail.com>
2022-03-04 12:10:04 +0100

    updated version colleagues

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+from kidney_kids.data import get_cleaned_data
+from kidney_kids.randomforest import RandomForest
+
+X_train, X_test, y_train, y_test = get_cleaned_data(path = 'raw_data/kidney_disease.csv')
+forest_model = RandomForest()
+X_preproc = forest_model.preproc(X_train)
+
 
 # Title/Text
-st.title('Chronic Kidney Disease')
-st.text('Built with Streamlit')
+# st.title('Chronic Kidney Disease')
+# st.text('Built with Streamlit')
 
-# Header and Subheader
-st.header('EDA Section')
-st.subheader('kidney_disease.csv')
+# # Header and Subheader
+# st.header('EDA Section')
+# st.subheader('kidney_disease.csv')
 
-# Checkbox
-if st.checkbox("Show Dataset"):
-    st.text('Showing Dataset')
+# # Checkbox
+# if st.checkbox("Show Dataset"):
+#     st.text('Showing Dataset')
 
-# Selection
-appetite = st.selectbox('Appetite', ('Good','Poor'))
+# # Selection
+# appetite = st.selectbox('Appetite', ('Good','Poor'))
 
-# Sliders
-age = st.slider('Your Age',2,90)
+# # Sliders
+# age = st.slider('Your Age',2,90)
 
-# Buttons
-if st.button('About Us'):
-    st.text('Kidney Kids 2022')
+# # Buttons
+# if st.button('About Us'):
+#     st.text('Kidney Kids 2022')
+
+if __name__ == '__main__':
+    print(X_preproc)
